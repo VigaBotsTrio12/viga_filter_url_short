0b66c286be2e62e1a5ba42173a335bd7b8f2ac70
Vikram Singh <122893366+VigaBotsTrio12@users.noreply.github.com>
2023-01-30 09:22:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kdbotz/Lucifer.git /Lucifer
+  git clone https://github.com/VigaBotsTrio12/viga_filter_url_short.git /Lucifer
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Lucifer
